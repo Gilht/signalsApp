77b6aaff7f0df3448c767a68587eb107f09f1cfa
Gilht <ghdzt14@hotmail.com>
2024-09-24 11:51:33 -0600

    added a directive  to handle errors and show each error in a message

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
@@ -7,6 +8,12 @@ export class CustomLabelDirective implements OnInit{
 
   private htmlElement?: ElementRef<HTMLElement>;
   private _color: string = "red";
+  private _errors?: ValidationErrors | null;
+
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorMessage();
+  }
 
   @Input() set color(value: string){
     this._color = value; 
@@ -29,5 +36,34 @@ export class CustomLabelDirective implements OnInit{
     if (!this.htmlElement) return ; 
     this.htmlElement!.nativeElement.style.color = this._color;
    }
+
+   setErrorMessage(): void {
+    if(!this.htmlElement) return;
+    if(!this._errors) {
+      this.htmlElement.nativeElement.innerText = "No errors";
+      return;
+    }
+
+    const formErrors = Object.keys(this._errors);
+
+    if(formErrors.includes("required")) {
+      this.htmlElement.nativeElement.innerText = "El campo es requerido";
+      return;
+    }
+
+    if(formErrors.includes("minlength")) {
+      const min = this._errors!["minlength"]["requiredLength"];
+      const current = this._errors!["minlength"]["actualLength"];
+      this.htmlElement.nativeElement.innerText = `Minimo ${current}/${min}caracteres.`;
+      return;
+    }
+
+    if(formErrors.includes("email")) {
+      this.htmlElement.nativeElement.innerText = "Email invalid format";
+      return;
+    }
+
+   }
+
 
 }
